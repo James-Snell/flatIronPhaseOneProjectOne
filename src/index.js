fae4419e3521d98abd3284cd6c636b67d028e2e0
Justin Steffen <jcsteffen03@gmail.com>
2024-06-13 14:59:45 -0600

    blueprint code

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@
 fetch("https://api.openbrewerydb.org/breweries")
 .then(r=> r.json())
 .then(bars =>{
+    //main function holding all event listeners
     console.log(bars)
 })
+
+//search function that allows users to input city,zip,and type, and populating 1 random bar within the parameters, in the results box everytime  
+
+
+
+//submit function to allow user to sumbit an "Experince", with a rating, comments, and picture, function also populates name of bar in likes box.
+
+
+
+//focus function that allows users to focus over liked bars and populate the data in 2nd likes bar with bar data as well as user rating and comments and photos.
+
+
+
+//main function that has a submit, click, and focus event listener that calls the function everytime. 
